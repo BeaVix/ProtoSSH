ced8fadd6f5e6f6d2305787c9e1a234f8051273c
BeaVix <98414235+BeaVix@users.noreply.github.com>
2024-01-16 21:42:13 -0300

    No more password required

--- helpers/loadConfig.py
+++ helpers/loadConfig.py
@@ -4,7 +4,6 @@ from .Data import Data
 def loadConfig(data:Data):
     file = loadJson('config')
     data.setUser(file["user"])
-    data.setPswd(file["pswd"])
     data.setDom(file["dom"])
     data.setPerf(file["perf"])
     data.setServer(file["srvr"])
